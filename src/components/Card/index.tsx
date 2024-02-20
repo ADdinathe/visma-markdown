7910424801164f37e0137ad8ab021d6d39c6b885
a.shumakov <a.shumakov@ktsstudio.ru>
2024-02-20 21:19:34 +0300

    wip: ui

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Card.scss';
+import cn from 'classnames';
+
+export enum CardColorEnum {
+    blue = 'blue',
+    purple = 'purple'
+}
+
+export type CardProps = {
+    children:  React.ReactNode;
+    color?: CardColorEnum;
+    tag: React.ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({tag, color = CardColorEnum.blue, children})=>{
+
+    return (
+        <div className={cn('card', `card_${color}`)}>
+            <div className="tag">{tag}</div>
+            <div className="text">{children}</div>
+        </div>
+    )
+}
+
+export default React.memo(Card);
